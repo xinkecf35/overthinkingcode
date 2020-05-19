ccab534cc3b3c85d7b2caf920ba3aba8f1b18f8e
Xinke Chen <xinkecf35@gmail.com>
2020-05-18 23:13:34 -0400

    Added regex to ignore long svg paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
@@ -15,11 +15,18 @@ module.exports = {
     'plugin:nuxt/recommended',
     'google',
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    semi: [2, "always"],
-  }
-}
+    'max-len': [
+      2,
+      {
+        code: 80,
+        ignoreTemplateLiterals: true,
+        ignoreStrings: true,
+        ignorePattern: ".*d=\".+\""
+      },
+    ],
+    semi: [2, 'always'],
+  },
+};
