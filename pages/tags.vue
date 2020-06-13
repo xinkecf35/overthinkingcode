455c658739bfead9426d816f4d353dcefe1edc08
Xinke Chen <xinkecf35@gmail.com>
2020-06-13 17:11:44 -0400

    Started tags page; displays posts by associated tags

---
+++ pages/tags.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <ul v-for="tag in Object.keys(tags)" :key="tag">
+      <li>
+        <section>
+          {{ tag }}
+          <ul v-for="post in tags[tag]" :key="post.id">
+            <li>
+              <nuxt-link :to="post.route">{{ post.title }} </nuxt-link>
+            </li>
+          </ul>
+        </section>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: mapState(['tags']),
+};
+</script>
+
+<style></style>
