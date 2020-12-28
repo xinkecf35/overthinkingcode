431bbe2ebe48fc4dc8ccf4691680a82ab02d8c06
Xinke Chen <xinkecf35@gmail.com>
2020-12-28 13:58:21 -0500

    Fix up with about page

--- pages/about.vue
+++ pages/about.vue
@@ -4,27 +4,26 @@
       <h1>About</h1>
       <div class="about-row">
         <p>
-          I am Xinke Chen, webmaster and author of this blog you're currently on
+          I am Xinke Chen, webmaster and author of the blog you're currently on
           right now. I have a B.S. Computer Science from the University of
           Pittsburgh with a Minor in Economics. As you might expect, I care
-          quite about technology, specifically, in experimenting with it. In my
-          journey in becoming a better software engineer, I have played with and
-          tried to learn quite a few different frameworks, languages and
-          ecosystems. From iOS with Objective-C and Swift, to PostgreSQL and Vue
-          with JavaScript, my curiosity with technology and software is quite
-          diverse.
+          quite about technology and experimenting with it. In my journey in
+          becoming a better software engineer, I have played with and tried to
+          learn quite a few different frameworks, languages and ecosystems. From
+          iOS with Objective-C and Swift, to PostgreSQL and Vue with JavaScript,
+          my curiosity with technology and software is quite diverse.
         </p>
         <img src="~/assets/images/profile.jpg" alt="picture of Xinke Chen" />
       </div>
       <div class="about-row">
         <p>
-          Beyond just computers, I also love photography and specifically, astro
+          Beyond just computers, I also love photography, specifically, astro
           and night landscapes. Again, along with the theme of experimenting
-          (and also as a constraint of my location), I like these type of
-          subjects specifically to see how far I push the camera technically, to
-          see what even the eye can't see. There's something special in
-          capturing something that otherwise would not exist were it not for the
-          presence and ability of the camera.
+          (and as a constraint of my location), I like these type of subjects
+          specifically to see how far I push the camera technically, to see what
+          even the eye can't see. There's something special in capturing
+          something that otherwise would not exist were it not for the presence
+          and ability of the camera.
         </p>
         <img
           src="~/assets/images/cathedral-startrails.jpg"
@@ -37,23 +36,22 @@
           <p>
             Overthinking Code, the blog itself, is largely created and devised
             as a journaling device for myself; to keep track of tools and things
-            I find usefu or interesting and thereby maybe of use in the future.
-            Additionally, as an journaling tool, it's also a place for me to
-            reflect on my own career, albeit a reflection with a heavy technical
-            focus.
+            I find useful or interesting and thereby may be of use in the
+            future. Additionally, the blog is also a place for me to reflect on
+            my own career, albeit a reflection with a heavy technical focus.
           </p>
         </div>
       </div>
       <div class="about-row">
         <p>
           The title, is also a reflection of a personal habit of one, one that's
-          argubly not great but also has been one of the causes as to why I've
+          arguably not great but also has been one of the causes as to why I've
           experimented with so much. As a result of overthinking an issue or
           problem, I pursued a great deal of solutions and implementations that
-          often while overbuilt or could've been down simpler have down the line
-          been incredibly useful later on. I believe that effectively asking
-          "what if?" is a good thing, and something I feel is worth encouraging
-          (in that right circumstances of course).
+          often while overbuilt or could've been done simpler which have down
+          the line been incredibly useful later on. I believe that effectively
+          asking "what if?" is a good thing, and something I feel is worth
+          encouraging (in the right circumstances of course).
         </p>
       </div>
     </article>
